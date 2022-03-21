6a20eba7bc09af7afff2b15f623a81a480b3c222
Nhut Doan <znhut12z@gmail.com>
2022-03-21 12:28:33 +0700

    update appjs

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server, {
 	cors: {
-		origin: 'http://localhost:3000'
-		// origin: 'https://relaxed-tesla-197163.netlify.app'
+		origin: ['http://localhost:3000', 'https://relaxed-tesla-197163.netlify.app']
 	}
 })
 const router = require('./router') 
